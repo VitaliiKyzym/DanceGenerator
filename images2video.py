6f3957194ba5a4c77ff8ab7c5fcd22cb94cd62e3
Vitsuji <vitsuji123@gmail.com>
2020-10-20 23:57:43 +0300

    Working Prototype

---
+++ images2video.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import glob
+import os
+from os.path import isfile, join
+import re
+
+def Images2Video (pathIn, pathOut, frameSize):
+
+    # frameSize = (256, 256)
+    # i = 3
+    # pathIn = f'./generated/rnn/{i}/'
+    # pathOut = f'video{i}.avi'
+
+    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+    files.sort(key=lambda f: int(re.sub('\D', '', f)))
+
+    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), 20, frameSize)
+
+    for filename in files:
+        img = cv2.imread(pathIn + filename)
+        out.write(img)
+
+    out.release()
